b335505f459a87c76566cd65a3a79dc0a4fba456
amarucci89 <alexmarucci89@gmail.com>
2025-02-03 12:20:34 -0500

    Completed 2.83 - 2.88

--- Book.java
+++ Book.java
@@ -3,24 +3,64 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Alessandro Marucci
+ * @version 2025.02.03
  */
-class Book
+public class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages; // 2.85
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int numberOfPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = numberOfPages; // 2.85
+        String refNumber = ""; //2.88
+        
     }
 
     // Add the methods here ...
+    public void printAuthor() // 2.84
+    {
+        System.out.println(author);
+    }
+    
+    public void printTitle() // 2.84
+    {
+        System.out.println(title);
+    }
+    
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages() // 2.85
+    {
+        return pages;
+    }
+    
+    public void printDetails() // 2.87
+    {
+        System.out.println("Title: " + title + ", " + "Author: " + author + 
+        ", " + "Pages: " + pages);
+        
+    }
+    
+    public void setRefNumber (String ref) // 2.88
+    {
+    }
+    
 }
